019d59382eea37289e8a8bc74a97feb6f202d8aa
Ozelot <29740364+0zelot@users.noreply.github.com>
2023-03-29 20:51:06 +0200

    Added flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 module.exports = {
     content: [
+        "./index.html",
         "./src/page/**/*.{vue,html,js}",
+        "./node_modules/flowbite/**/*.js"
     ],
     theme: {
         extend: {}
     },
-    plugins: []
+    plugins: [
+        require("flowbite/plugin")
+    ]
 }
