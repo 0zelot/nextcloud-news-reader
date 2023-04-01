14e3f0a02a0d6fbb4d57b833f1b26db79be4e985
Ozelot <29740364+0zelot@users.noreply.github.com>
2023-04-01 19:41:46 +0200

    Initialized vue-router

--- src/page/main.js
+++ src/page/main.js
@@ -1,7 +1,13 @@
 import { createApp } from "vue";
+import { createWebHashHistory, createRouter } from "vue-router";
 
 import App from "./App.vue";
 
 import "./index.css";
 
-createApp(App).mount("#app");
+createApp(App)
+    .use(createRouter({
+        history: createWebHashHistory(),
+        routes: []
+    }))
+    .mount("#app");
