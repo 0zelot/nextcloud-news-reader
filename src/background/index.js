7ad6709957b7f13c5c629190afdc0f1948afcf99
Ozelot <29740364+0zelot@users.noreply.github.com>
2023-04-02 20:56:45 +0200

    Added auto-refresh shedule

--- src/background/index.js
+++ src/background/index.js
@@ -1,1 +1,7 @@
+import refreshItemData from "../utils/refreshItemData.js";
+
+chrome.alarms.onAlarm.addListener(e => refreshItemData());
+
+chrome.alarms.create("", {periodInMinutes: 1}); // TODO: Change it
+
 chrome.action.onClicked.addListener(e => chrome.tabs.create({url: chrome.runtime.getURL("src/page/index.html")}));
