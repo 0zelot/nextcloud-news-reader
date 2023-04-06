c8044708c230f11759a0f5555a2690ac97f5cc27
Ozelot <29740364+0zelot@users.noreply.github.com>
2023-04-06 17:11:19 +0200

    Removed /STARRED

--- src/page/components/Sidebar.vue
+++ src/page/components/Sidebar.vue
@@ -25,15 +25,6 @@
                     </li>
 
                     <li>
-                        <a href="#STARRED" class="flex items-center p-1 rounded-lg text-white hover:bg-gray-700 pl-3">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5">
-                                <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z" clip-rule="evenodd" />
-                            </svg>
-                            <span class="ml-1">Starred</span>
-                        </a>
-                    </li>
-
-                    <li>
                         <a href="#" data-modal-target="add-new-modal" data-modal-toggle="add-new-modal" class="flex items-center p-1 rounded-lg text-white hover:bg-gray-700 pl-3">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5">
                                 <path fill-rule="evenodd" d="M12 3.75a.75.75 0 01.75.75v6.75h6.75a.75.75 0 010 1.5h-6.75v6.75a.75.75 0 01-1.5 0v-6.75H4.5a.75.75 0 010-1.5h6.75V4.5a.75.75 0 01.75-.75z" clip-rule="evenodd" />
